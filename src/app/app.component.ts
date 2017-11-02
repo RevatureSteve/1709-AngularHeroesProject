2395da8fc44f0231c5fdf892507a2e7a6a33a2d1
Steven Kelsey <steven.kelsey@revature.com>
2017-11-02 10:36:07 -0400

    Step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 export class Hero {
   id: number;
   name: string;
 }
-
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 /*
  *
  * Component Decorator:
@@ -15,10 +27,13 @@ export class Hero {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Heroes application';
-  hero: Hero = {
-    id: 1,
-   name: 'Windstorm'
-  };
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
